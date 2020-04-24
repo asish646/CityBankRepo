7b1138eb00d35e52156d3345aca25f31d333057e
DEV2 <DEV2@gmail.com>
2020-04-24 16:42:50 +0530

    utility class m1() method by dev1

--- CityBankDemo/src/com/nit/beans/Utility.java
+++ CityBankDemo/src/com/nit/beans/Utility.java
@@ -8,6 +8,11 @@ public class Utility {
 		System.out.println("This is getDate method : 1234");
 		System.out.println("This is the second change...");
 		return new Date();
-		
 	}
+	
+	public void m1() {
+		System.out.println("Utility.m1()");
+	}
+	
+
 }
